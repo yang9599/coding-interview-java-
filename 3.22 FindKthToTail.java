2e7a1dcde5ddb740994624350affa23a76ee5739
Yang <49385126+yang9599@users.noreply.github.com>
2019-06-21 17:08:19 +0800

    Create 3.22 FindKthToTail.java

---
+++ 3.22 FindKthToTail.java
@@ -0,0 +1,31 @@
+class ListNode {
+	int val;
+	ListNode next = null;
+	ListNode(){
+		
+	}
+	ListNode(int val) {
+		this.val = val;
+	}
+}
+
+public class FindKthToTail {
+	public ListNode find(ListNode head, int k) {
+		if(head == null || k <= 0)
+			return null;
+		ListNode fast = head;
+		ListNode slow = head;
+		while(k-- > 1) {
+			if(fast.next != null)
+				fast = fast.next;
+			else {
+				return null;
+			}
+		}
+		while(fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+		return slow;
+	}
+}
