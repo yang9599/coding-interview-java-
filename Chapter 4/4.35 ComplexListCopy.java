54ee8fbb3a29ef7bf440de25847b7ae215e88e27
Yang <49385126+yang9599@users.noreply.github.com>
2019-06-26 21:59:38 +0800

    Update 4.35 ComplexListCopy.java

--- Chapter 4/4.35 ComplexListCopy.java
+++ Chapter 4/4.35 ComplexListCopy.java
@@ -13,6 +13,7 @@ public class ComplexList {
 			this.m_pSibling = m_pSibling;
 		}
 	}
+	//根据原始链表的每个节点N创建对应的N’
 	public void CloneNodes(ComplexListNode pHead) {
 		ComplexListNode pNode = pHead;
 		while(pNode != null) {
@@ -25,6 +26,7 @@ public class ComplexList {
 			pNode = pCloned.m_pNext;
 		}
 	}
+	//设置复制出来的节点的m_pSibling
 	public void ConnectSiblingNodes(ComplexListNode pHead) {
 		ComplexListNode pNode = pHead;
 		while(pNode != pHead) {
@@ -35,6 +37,7 @@ public class ComplexList {
 			pNode = pCloned.m_pNext;
 		}
 	}
+	//把这个长链表拆分成两个链表
 	public ComplexListNode ReconnectNodes(ComplexListNode pHead) {
 		ComplexListNode pNode = pHead;
 		ComplexListNode pClonedHead = null;
@@ -52,6 +55,7 @@ public class ComplexList {
 		}
 		return pClonedHead;
 	}
+	//上面三步合并
 	public ComplexListNode Clone(ComplexListNode pHead) {
 		CloneNodes(pHead);
 		ConnectSiblingNodes(pHead);
