f9813a54509d90838180e882a1c700981f51409b
Yang <49385126+yang9599@users.noreply.github.com>
2019-06-30 23:11:27 +0800

    Create 6.62 LastRemaining.java

---
+++ Chapter 6/6.62 LastRemaining.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class CircleNumber {
+	public int LastRemaining(int n, int m) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int bt = 0;
+		for(int i=0; i<n; i++)
+			list.add(i);
+		while(list.size()>1) {
+			bt = (bt+m+1) % list.size();
+			list.remove(bt);
+		}
+		return list.size()==1?list.get(0):-1;
+	}
+
+}
