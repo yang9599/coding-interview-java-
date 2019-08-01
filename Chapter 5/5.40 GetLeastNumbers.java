a45c4b9a182f1790f6681789767e33f77fc51926
Yang <49385126+yang9599@users.noreply.github.com>
2019-08-01 22:58:37 +0800

    Update 5.40 GetLeastNumbers.java

--- Chapter 5/5.40 GetLeastNumbers.java
+++ Chapter 5/5.40 GetLeastNumbers.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class FindKthMin {
-	public ArrayList<Integer> GetLeastNumbers(int[] input, int k) {
+	// 创建大根堆的方法，时间复杂度为nlogk，适合处理海量数据
+	public ArrayList<Integer> GetLeastNumbers1(int[] input, int k) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		if(input == null || k<=0 || k>input.length)
 			return null;
@@ -40,6 +41,45 @@ public class FindKthMin {
 			input[i] = input[max];
 			input[max] = temp;
 			maxHeap(input, max);
+		}
+	}
+	//使用Partition方法，时间复杂度为n
+	private static int Partition(int[] arr, int start, int end) {
+		int key = arr[start];
+		while(start < end) {
+			while(arr[end] >= key && end > start)
+				end--;
+			arr[start] = arr[end];
+			while(arr[start] <= key && end > start)
+				start++;
+			arr[end] = arr[start];
+		}
+		arr[start] = key;
+		return start;
+	}	
+	public static void GetLeastNumbers2(int[] input, int k) {
+		int len = input.length;
+		if(input==null || k>len || len<=0 || k<=0)
+			return;
+		int start = 0;
+		int end = len-1;
+		int index = Partition(input, start, end);
+		while(index != k-1) {
+			if(index > k-1) {
+				end = index-1;
+				index = Partition(input, start, end);
+			}
+			else {
+				start = index+1;
+				index = Partition(input, start, end);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int[] arr = {2,8,4,11,6,10,5,18,3,1};
+		GetLeastNumbers2(arr, 5);
+		for(int i=0; i<5; i++) {
+			System.out.println(arr[i]);
 		}
 	}
 
